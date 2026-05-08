ef027fad9961576d628b0545aba5e15b9eb0f66a
kmalaver <kevinmalaver@gmail.com>
2026-05-07 19:19:44 -0500

    agregar clase sistema

--- entidades.py
+++ entidades.py
@@ -1,10 +1,46 @@
+from errores import DatoInvalidoError
 from abc import ABC 
 
-class Entidades_generales(ABC):
-    pass
+class EntidadGeneral(ABC):
+    def __init__(self, id):
+        self.id = id
 
-class Cliente(Entidades_generales):
+class Cliente(EntidadGeneral):
     def __init__(self, nombre, documento, email):
+        if not nombre:
+            raise DatoInvalidoError("El nombre del cliente no puede estar vacío.")
         self.nombre = nombre
         self.documento = documento
         self.email = email
+
+
+
+# Servicios
+class Servicio(EntidadGeneral):
+    pass
+
+
+class ServicioA(Servicio):
+    pass
+
+class ServicioB(Servicio):
+    pass
+
+class ServicioC(Servicio):
+    pass
+
+
+
+# Reservas
+
+class Reserva:
+    def __init__(self, cliente, servicio, duracion, estado):
+        self.cliente = cliente
+        self.servicio = servicio
+        self.duracion = duracion
+        self.estado = estado
+
+    def confirmar(self):
+        self.estado = "Confirmada"
+
+
