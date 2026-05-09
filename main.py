08ef41d13d749e084839ee3e64556d70c812fe09
kmalaver <kevinmalaver@gmail.com>
2026-05-09 17:36:35 -0500

    agregar la estructura inicial de la interfaz con tkinter

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
+from ui import crear_app
 
-# ejemplo de importación de modulos personalizados
-from logger import Logger
-from sistema import Sistema
-import entidades
-
-sistema = Sistema()
-
-# Agregar servicios
-sistema.agregar_servicio(entidades.ServicioReservaSalas(sala_numero=101, precio_hora=50.0))
-sistema.agregar_servicio(entidades.ServicioAlquilerEquipos(equipo_tipo="Proyector", precio_hora=20.0))
-sistema.agregar_servicio(entidades.ServicioAsesoriasEspecializadas(especialidad="Matemáticas", precio_hora=100.0))
-
-sistema.agregar_cliente(entidades.Cliente("Juan Pérez", "12345678", "juan@gmail.com"))
-
+app = crear_app()
+app.mainloop()
